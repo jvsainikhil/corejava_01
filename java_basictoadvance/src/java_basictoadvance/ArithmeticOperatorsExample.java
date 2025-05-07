9da49073fc154129c75b8d6ce5119682bc2b47e4
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-07 12:23:35 +0530

    ArithmeticOperatorsExample

---
+++ java_basictoadvance/src/java_basictoadvance/ArithmeticOperatorsExample.java
@@ -0,0 +1,23 @@
+package java_basictoadvance;
+
+public class ArithmeticOperatorsExample {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 3;
+
+        // Division
+        float resultDiv = a / b; // Integer division
+        double resultDivFloat = (double) a / b; // Floating-point division
+
+        // Modulus
+        float resultMod = a % b;
+
+        System.out.println("Integer Division (10 / 3) = " + resultDiv);
+        System.out.println("Floating Point Division (10.0 / 3) = " + resultDivFloat);
+        System.out.println("Modulus (10 % 3) = " + resultMod);
+        System.out.printf("this is the out put %d",a);
+    }
+}//
+
+   // - %d which is used to insert any value in middle in according to an given order it will be assigned, 
+                                                                                                         
