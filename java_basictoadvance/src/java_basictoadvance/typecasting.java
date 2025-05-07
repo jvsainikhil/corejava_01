4b689047f307cef9673ba1025762cb03ef6154ec
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-07 12:28:02 +0530

    typecasting

---
+++ java_basictoadvance/src/java_basictoadvance/typecasting.java
@@ -0,0 +1,23 @@
+package java_basictoadvance;
+
+public class typecasting {
+	public static void main(String []args) {
+		byte ty1=127;
+		int end1 = ty1;
+		
+		// converting int to byte if range increases 
+		byte a=(byte)end1; //type casting
+		System.out.println(a);
+		
+		int ty2=257;
+		//if range increases it will use modules 
+		byte b=(byte)ty2; //type casting
+		System.out.println(b);
+		
+		//implicitly
+		byte qw=10;
+		byte wq=30;
+		int result = qw*wq;
+		System.out.println(result);
+	}
+}
