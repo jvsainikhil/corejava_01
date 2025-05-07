075e5d3f4884e5441971cec5832b67895806c600
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-07 11:10:41 +0530

    Checking the code push

---
+++ java_basictoadvance/src/java_basictoadvance/Java_all.java
@@ -0,0 +1,10 @@
+package java_basictoadvance;
+
+public class Java_all {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
