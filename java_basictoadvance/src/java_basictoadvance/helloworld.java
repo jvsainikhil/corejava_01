22c6e5fbc69fab19c80e049fba3745ec4ab89803
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-07 12:26:05 +0530

    helloworld

---
+++ java_basictoadvance/src/java_basictoadvance/helloworld.java
@@ -0,0 +1,14 @@
+package java_basictoadvance;
+
+public class helloworld {
+	public static void main(String args[]) {
+		int num1=3;
+		int num2=4;
+		int add=num1+num2;
+		System.out.println(add);	
+		
+		//another class
+		float a=5.4f;
+		System.out.println(a);
+	}
+}
