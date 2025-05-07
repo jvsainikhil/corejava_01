77bd1c78aa7a78a34c551306b6e256766b627acc
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-07 12:09:32 +0530

    commit

--- java_basictoadvance/src/java_basictoadvance/Java_all.java
+++ java_basictoadvance/src/java_basictoadvance/Java_all.java
@@ -4,7 +4,7 @@ public class Java_all {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("hello world - 0");
 	}
 
 }
