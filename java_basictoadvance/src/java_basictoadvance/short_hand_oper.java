cb2faf99c466bda91835ca027a53eb3773474b95
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-07 12:26:51 +0530

    short_hand_oper

---
+++ java_basictoadvance/src/java_basictoadvance/short_hand_oper.java
@@ -0,0 +1,14 @@
+package java_basictoadvance;
+
+public class short_hand_oper {
+	public static void main(String []ards) {
+		int i=2,b=3;
+		i+=2; //add
+		i-=2; //sub
+		i*=2; //multiply 
+		b%=2; //
+		System.out.println(b);
+		i/=2;
+		System.out.println(i);		
+	}
+}
