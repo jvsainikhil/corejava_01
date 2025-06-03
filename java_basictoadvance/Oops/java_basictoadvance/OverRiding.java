68a623da1c4415eebdec921b4e18d4609043c748
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-03 18:22:20 +0530

    01-methods

--- java_basictoadvance/Oops/java_basictoadvance/OverRiding.java
+++ java_basictoadvance/Oops/java_basictoadvance/OverRiding.java
@@ -1,42 +1,55 @@
 package java_basictoadvance;
 
-class Animal {
-	void makeSound() {
-		System.out.println("Animal makes a sound");
+class Computer {
+	//	public void makeSound() {
+	//		System.out.println("Animal makes a sound");
+	//	}
+	public void playMusic()
+	{
+		System.out.println("Music Playing..");
+	}
+	public String getMEAPen(int cost) {
+		if (cost>=10)
+			return "Pen";
+		else
+			return "null";
 	}
 }
-
-class Dog extends Animal {
-	@Override
-	void makeSound() {
-		System.out.println("Dog barks");
-	}
-}
-
-class Cat extends Animal{
-	@Override
-	void makeSound() {
-		System.out.println("Cat meow");
-	}
-}
-
-class Cow extends Animal{
-	@Override
-	void makeSound() {
-		System.out.println("Cat Amba");
-	}
-}
+//class Dog extends Animal {
+//	@Override
+//	void makeSound() {
+//		System.out.println("Dog barks");
+//	}
+//}
+//
+//class Cat extends Animal{
+//	@Override
+//	void makeSound() {
+//		System.out.println("Cat meow");
+//	}
+//}
+//
+//class Cow extends Animal{
+//	@Override
+//	void makeSound() {
+//		System.out.println("Cat Amba");
+//	}
+//}
 
 public class OverRiding {
 	public static void main(String[] args) {
-		Animal myDog = new Dog(); // Polymorphism
-		myDog.makeSound();        // Output: Dog barks
+		Computer obj =new Computer();
+		String str = obj.getMEAPen(20);
+		System.out.println(str);
 
-		Animal cat = new Cat();
-		cat.makeSound();
-		
-		Animal Cow = new Cow();
-		Cow.makeSound();
+		//		Animal myDog = new Dog(); // Polymorphism
+		//		myDog.makeSound();        // Output: Dog barks
+		//
+		//		Animal cat = new Cat();
+		//		cat.makeSound();
+		//		
+		//		Animal Cow = new Cow();
+		//		Cow.makeSound();
 	}
 
 
