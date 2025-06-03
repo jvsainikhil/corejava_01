098230e6e3c8b62e28254833fa8b4b96335619cf
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-03 18:03:11 +0530

    OverRiding

--- java_basictoadvance/Oops/java_basictoadvance/OverRiding.java
+++ java_basictoadvance/Oops/java_basictoadvance/OverRiding.java
@@ -1,31 +1,43 @@
 package java_basictoadvance;
 
 class Animal {
-    void makeSound() {
-        System.out.println("Animal makes a sound");
-    }
-    
-}
-//@FunctionalInterface
-interface Animals {
-    void makeSound();
+	void makeSound() {
+		System.out.println("Animal makes a sound");
+	}
 }
 
 class Dog extends Animal {
-    @Override
-    void makeSound() {
-        System.out.println("Dog barks");
-    }
+	@Override
+	void makeSound() {
+		System.out.println("Dog barks");
+	}
+}
+
+class Cat extends Animal{
+	@Override
+	void makeSound() {
+		System.out.println("Cat meow");
+	}
+}
+
+class Cow extends Animal{
+	@Override
+	void makeSound() {
+		System.out.println("Cat Amba");
+	}
 }
 
 public class OverRiding {
-    public static void main(String[] args) {
-        Animal myDog = new Dog(); // Polymorphism
-        myDog.makeSound();        // Output: Dog barks
-        
-        Animals cat = () -> System.out.println("cat barks");
-        cat.makeSound();
-    }
-    
-   
+	public static void main(String[] args) {
+		Animal myDog = new Dog(); // Polymorphism
+		myDog.makeSound();        // Output: Dog barks
+
+		Animal cat = new Cat();
+		cat.makeSound();
+		
+		Animal Cow = new Cow();
+		Cow.makeSound();
+	}
+
+
 }
