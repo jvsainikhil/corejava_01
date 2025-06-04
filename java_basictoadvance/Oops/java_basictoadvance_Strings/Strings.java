05cbf4ffd480793719f5d3c940c576ad02deeed8
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-04 15:03:43 +0530

    adding few methods

--- java_basictoadvance/Oops/java_basictoadvance_Strings/Strings.java
+++ java_basictoadvance/Oops/java_basictoadvance_Strings/Strings.java
@@ -2,9 +2,16 @@ package java_basictoadvance_Strings;
 
 public class Strings{
 	public static void main(String[] args) {
+		//StringBuffer
 		StringBuffer a=new StringBuffer("Nikhil");
 		a.length();
 		a.deleteCharAt(0);
-	    System.out.println(a);
-		}
+		a.setLength(20);
+		System.out.println(a);
+		//StringBuilder
+		StringBuilder sb = new StringBuilder("Hello");
+		sb.append(" World");
+		System.out.println("StringBuilder result: " + sb.toString());
+	}
 }
+
