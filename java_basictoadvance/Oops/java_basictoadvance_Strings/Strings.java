2f7b2c0a4df16517d865c75c8290752fd1027e60
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-04 13:50:05 +0530

    String01

---
+++ java_basictoadvance/Oops/java_basictoadvance_Strings/Strings.java
@@ -0,0 +1,10 @@
+package java_basictoadvance_Strings;
+
+public class Strings{
+	public static void main(String[] args) {
+		StringBuffer a=new StringBuffer("Nikhil");
+		a.length();
+		a.deleteCharAt(0);
+	    System.out.println(a);
+		}
+}
