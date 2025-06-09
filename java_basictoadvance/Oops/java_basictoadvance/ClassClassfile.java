c8856d4c1f7a320ca819bc009b4eb58f6dd5c491
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-09 21:55:06 +0530

    class.class
    //we are targeting to call class with out an object

---
+++ java_basictoadvance/Oops/java_basictoadvance/ClassClassfile.java
@@ -0,0 +1,36 @@
+package java_basictoadvance;
+
+//we are targeting to call class with out an object 
+class MobilePhone {
+    String brand;        // Instance variable
+    int price;           // Instance variable
+    static String name;  // Static variable shared by all objects
+
+    // Static block
+    static {
+        name = "Phone";
+        System.out.println("Static Block: name=" + name);
+    }
+
+    // Constructor
+    public MobilePhone() {
+        brand = "";
+        price = 200;
+        System.out.println("Constructor called");
+    }
+
+    // Non-static method (instance method)
+    public void show1() {
+        System.out.println("brand=" + brand + ",\t price=" + price + ",\t name=" + name);
+    }
+}
+
+public class ClassClassfile {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        // Calling static block using reflection
+        Class.forName("java_basictoadvance.MobilePhone");
+    }
+}
+
+	
