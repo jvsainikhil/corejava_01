7ee34a8ec3a0c1eb601130811d404b7214b61d21
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-10 12:11:15 +0530

    Encapsulation 03

--- java_basictoadvance/Oops/java_basictoadvance_4pillors/Encapsulation.java
+++ java_basictoadvance/Oops/java_basictoadvance_4pillors/Encapsulation.java
@@ -1,24 +1,30 @@
 package java_basictoadvance_4pillors;
 
-//By using getters
+//By using getters and setters get and set
 class Human{
-	private int age = 25; // Accessible only in the same class
-	private String name = "Nikhil";
+	private int age; // Accessible only in the same class
+	private String name;
 	
 	public int getAge() {
 		return age;
 	}
+	public void setAge(int b) {
+		 age = b;
+	}
 	
 	public String getName() {
 		return name;
+	}
+	public void setName(String c) {
+		name=c;
 	}
 }
 
 public class Encapsulation {
 	public static void main (String [] args) {
 		Human a= new Human();
-//		a.age=25;
-//		a.name="Nikhil";
+		a.setAge(25);
+		a.setName("Nikhil");
 		System.out.println(a.getAge()+a.getName());
 	}
 }
