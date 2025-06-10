114deaa109df3588925404aacb2f5c6589a3adc8
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-10 12:28:57 +0530

    this 01

--- java_basictoadvance/Oops/java_basictoadvance_4pillors/Encapsulation.java
+++ java_basictoadvance/Oops/java_basictoadvance_4pillors/Encapsulation.java
@@ -10,8 +10,9 @@ class Human{
 	public int getAge() {
 		return age;
 	}
-	public void setAge(int age) {// local and instance is same 
-		age = age;// new key word this 
+	public void setAge(int age,Human a) {// local and instance is same 
+		Human obj1=a;
+		obj1.age = age;// assigning value to it self 
 	}
 	public String getName() {
 		return name;
@@ -24,8 +25,10 @@ class Human{
 public class Encapsulation {
 	public static void main (String [] args) {
 		Human a= new Human();
-		a.setAge(25);
+		a.setAge(25,a);// passing the object 2-times 
+		// by default it will give you as this
 		a.setName("Nikhil");
 		System.out.println(a.getAge()+a.getName());
+		//output: 0Nikhil
 	}
 }
