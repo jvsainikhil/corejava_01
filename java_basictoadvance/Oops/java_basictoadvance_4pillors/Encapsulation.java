b09a6459ee659b8a2ed99ff657af6e80f1048148
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-10 12:22:41 +0530

    this.Encapsulation 06

--- java_basictoadvance/Oops/java_basictoadvance_4pillors/Encapsulation.java
+++ java_basictoadvance/Oops/java_basictoadvance_4pillors/Encapsulation.java
@@ -2,6 +2,7 @@ package java_basictoadvance_4pillors;
 
 //By using getters and setters get and set, Encapsulation
 //This from source --> getters and setters shortcut
+//Every method will have it's own stack 
 class Human{
 	private int age; // Accessible only in the same class
 	private String name;
@@ -9,14 +10,14 @@ class Human{
 	public int getAge() {
 		return age;
 	}
-	public void setAge(int age) {
-		this.age = age;
+	public void setAge(int age) {// local and instance is same 
+		age = age;// new key word this 
 	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
-		this.name = name;
+		this.name = name;// new key word this 
 	}
 }
 
