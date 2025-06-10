58aeed5f272bb5a848cf7377847188135dc43f6d
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-10 12:05:38 +0530

    Encapsulation 02
    By using getters

--- java_basictoadvance/Oops/java_basictoadvance_4pillors/Encapsulation.java
+++ java_basictoadvance/Oops/java_basictoadvance_4pillors/Encapsulation.java
@@ -1,16 +1,24 @@
 package java_basictoadvance_4pillors;
 
-//General way we call data like this 
+//By using getters
 class Human{
-	int age;
-	String name;
+	private int age = 25; // Accessible only in the same class
+	private String name = "Nikhil";
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
 
 public class Encapsulation {
 	public static void main (String [] args) {
 		Human a= new Human();
-		a.age=25;
-		a.name="Nikhil";
-		System.out.println(a.age+a.name);
+//		a.age=25;
+//		a.name="Nikhil";
+		System.out.println(a.getAge()+a.getName());
 	}
 }
