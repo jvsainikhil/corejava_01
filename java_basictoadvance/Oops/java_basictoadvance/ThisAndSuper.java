6c7bdf736ba9b03f8f0aa47c8d9bd1b52995374e
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-23 10:33:27 +0530

    super parameter

--- java_basictoadvance/Oops/java_basictoadvance/ThisAndSuper.java
+++ java_basictoadvance/Oops/java_basictoadvance/ThisAndSuper.java
@@ -16,20 +16,26 @@ class B extends A{
 		super();//default //In every constructor this method will be there 
 		System.out.println("in B");
 	}
-	B(int b){//--------------- for this one 01
+	B(int b){//---------------> for this one 01
 		//call the constructor of a /*super class*/ the default one 
-		//super();//---------- for this one calling A 01 //default //In every constructor this method will be there 
-		super(b);//--------------- for this one calling A(int a) 02
+		//super();//----------> for this one calling A 01 //default //In every constructor this method will be there 
+		super(b);//-----------> for this one calling A(int a) 02
 		System.out.println("in B int: " + b);
 	}
 }
-
 
 public class ThisAndSuper {
 	public static void main(String[] args) {
 		//B obj = new B();
 		B obj1= new B(5);
-		/*
+		/*---->02
+		 * This is the output: 
+		 * in A int: 5 
+		 * in B int: 5
+		 * 
+		 */
+
+		/*---->01
 		 * This is the output: 
 		 * in A 
 		 * in B int: 5
