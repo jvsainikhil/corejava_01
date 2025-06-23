b19dadf35a6e6a3e169424f2973184e2b3a423f0
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-23 08:39:28 +0530

    error_Inheritance

--- java_basictoadvance/Oops/java_basictoadvance_4pillors/Inheritance.java
+++ java_basictoadvance/Oops/java_basictoadvance_4pillors/Inheritance.java
@@ -1,6 +1,6 @@
 package java_basictoadvance_4pillors;
 //file_01
-class Cal{
+class Calculator{
 	public int add(int n1,int n2) {
 		return n1+n2;
 	}
@@ -12,7 +12,7 @@ class Cal{
 }
 
 //file_03
-class AdvanceCal{
+class AdvanceCalculator{
 	public int multi(int n1,int n2) {
 		return n1*n2;
 	}
@@ -25,7 +25,7 @@ class AdvanceCal{
 //file_02
 public class Inheritance { // General way 
 	public static void main(String[] args) {
-    AdvanceCal obj=new AdvanceCal();
+    AdvanceCalculator obj=new AdvanceCalculator();
     //Cal obj=new Cal();
     int r1 =obj.add(4,5); // error
     int r2 =obj.sub(4,5); // error 
