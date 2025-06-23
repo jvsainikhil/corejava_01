013957c8799565f16cb49f9209ca3e433d651a0f
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-23 10:33:12 +0530

    Super calling parameter

--- java_basictoadvance/Oops/java_basictoadvance/ThisAndSuper.java
+++ java_basictoadvance/Oops/java_basictoadvance/ThisAndSuper.java
@@ -1,11 +1,11 @@
 package java_basictoadvance;
 
 class A{//super class
-	A(){//---------------> this is executing 
+	A(){//---------------> this is executing 01
 		super();//default //In every constructor this method will be there 
 		System.out.println("in A");
 	}
-	A(int a){
+	A(int a){ //---> if we want to call this 02
 		super();//default //In every constructor this method will be there 
 		System.out.println("in A int: " + a);
 	}
@@ -16,9 +16,10 @@ class B extends A{
 		super();//default //In every constructor this method will be there 
 		System.out.println("in B");
 	}
-	B(int b){//--------------- for this one 
+	B(int b){//--------------- for this one 01
 		//call the constructor of a /*super class*/ the default one 
-		super();//default //In every constructor this method will be there 
+		//super();//---------- for this one calling A 01 //default //In every constructor this method will be there 
+		super(b);//--------------- for this one calling A(int a) 02
 		System.out.println("in B int: " + b);
 	}
 }
