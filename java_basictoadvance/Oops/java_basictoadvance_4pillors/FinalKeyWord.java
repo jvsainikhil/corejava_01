ac6d5ae219ddbf5dab2445fca22fb7f4ef5d5651
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-25 20:23:19 +0530

    Final_01

---
+++ java_basictoadvance/Oops/java_basictoadvance_4pillors/FinalKeyWord.java
@@ -0,0 +1,10 @@
+package java_basictoadvance_4pillors;
+
+// final with variable
+public class FinalKeyWord {
+	public static void main(String[] args) {
+		int num =8;
+		num =9;
+		System.out.println(num); // output: 9 
+	}
+}
