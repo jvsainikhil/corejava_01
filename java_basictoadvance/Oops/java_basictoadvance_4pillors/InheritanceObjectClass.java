bf607c512284cd1686f05f2957d7d4487a26e8fa
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-25 22:13:46 +0530

    InheritanceObjectClass
    class object which returns class name.getname.hexa

---
+++ java_basictoadvance/Oops/java_basictoadvance_4pillors/InheritanceObjectClass.java
@@ -0,0 +1,16 @@
+package java_basictoadvance_4pillors;
+
+class DifferentLaptop {
+	 String model;
+	 int price;
+
+}
+
+public class InheritanceObjectClass {
+	public static void main(String[] args) {
+		DifferentLaptop obj = new DifferentLaptop();
+		obj.model = "Lenova" ;
+		obj.price = 10000;
+		System.out.println(obj);
+	}
+}
