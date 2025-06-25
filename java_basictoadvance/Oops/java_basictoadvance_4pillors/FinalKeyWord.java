8d8237e1c7300e5cccdff7b167c0654701d76641
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-25 20:23:58 +0530

    final got an error
    Exception in thread "main" java.lang.Error: Unresolved compilation
    problem:
    	The final local variable num cannot be assigned. It must be blank and
    not using a compound assignment
    
    	at java_basictoadvance/java_basictoadvance_4pillors.FinalKeyWord.main(FinalKeyWord.java:7)

--- java_basictoadvance/Oops/java_basictoadvance_4pillors/FinalKeyWord.java
+++ java_basictoadvance/Oops/java_basictoadvance_4pillors/FinalKeyWord.java
@@ -3,7 +3,7 @@ package java_basictoadvance_4pillors;
 // final with variable
 public class FinalKeyWord {
 	public static void main(String[] args) {
-		int num =8;
+		final int num =8;
 		num =9;
 		System.out.println(num); // output: 9 
 	}
