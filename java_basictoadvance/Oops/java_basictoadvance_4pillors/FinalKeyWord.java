99f61e4965b7722cd86a935d5d8932d18f0ecf78
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-25 20:30:45 +0530

    final class method

--- java_basictoadvance/Oops/java_basictoadvance_4pillors/FinalKeyWord.java
+++ java_basictoadvance/Oops/java_basictoadvance_4pillors/FinalKeyWord.java
@@ -1,10 +1,35 @@
 package java_basictoadvance_4pillors;
 
-// final with variable
+// final with variable:
+
+/*//final variable
+//✅ Once assigned, its value cannot be changed
+final int x = 10;
+
+//final method
+//✅ Cannot be overridden by subclasses
+final void display() {
+ System.out.println("Hello");
+}
+
+//final class
+//✅ Cannot be extended (no subclass allowed)
+final class MyClass {
+ // class content
+}
+*/
+final class calc {
+	public void show() {
+
+	}
+}
+
+class advancecalc extends Calc {// we will gwt error due to final
+
+}
+
 public class FinalKeyWord {
 	public static void main(String[] args) {
-		final int num =8;
-		num =9;
-		System.out.println(num); // output: error
+
 	}
 }
