f94eece085a78aaa483af66ab71551fb27cf24f7
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-26 12:18:37 +0530

    abstract methods

--- java_basictoadvance/Oops/java_basictoadvance_Objects/AbstractKeyWord.java
+++ java_basictoadvance/Oops/java_basictoadvance_Objects/AbstractKeyWord.java
@@ -1,26 +1,43 @@
 package java_basictoadvance_Objects;
 
-abstract class Car{
+abstract class Car{ //-------|1 // abstract class can have implemented and non implemented methods
 //	public void drive() {
 //		//not shure 
 //	}
-	public abstract void drive();
+	public abstract void drive();//-------|0,1 can create multiple 
+	public abstract void fly(); // not derived 
 	public void playMusic() {
 		System.out.println("Play music");
 	}
 }
 
-class WagonR extends Car{
-	public void drive() {
+abstract class WagonR extends Car{ // every abstract shoud be implemented if you dont want we need to do abstract
+	public void drive() {//--|0
 		System.out.println("drive");
 	}
+
+//	@Override
+//	public void fly() {
+//		// TODO Auto-generated method stub
+//	}
+}
+
+class updatedWagonR extends WagonR{  //concrete class
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		System.out.println("Fly");
+	}
+	
 }
 
 public class AbstractKeyWord {
 	public static void main(String[] args) {
-		 Car obj = new WagonR(); 
+		 Car obj = new updatedWagonR(); 
 		 // abstract car cannot create object but we can use reference
 		 obj.drive();
 		 obj.playMusic();
+		 obj.fly();
 	}
 }
