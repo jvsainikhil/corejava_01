7fdab3720c9b9b1c4f38978929f476eb354828d9
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-26 08:13:30 +0530

    WrapperClass_02

--- java_basictoadvance/Oops/java_basictoadvance_Objects/WrapperClass.java
+++ java_basictoadvance/Oops/java_basictoadvance_Objects/WrapperClass.java
@@ -3,9 +3,9 @@ package java_basictoadvance_Objects;
 public class WrapperClass {
 	public static void main(String[] args) {
 		
-		int num = 7;
-		Integer num1 = new Integer(8);
-		/*
+		int num = 7;//primitive
+		Integer num1 = new Integer(8);//reference variable
+		/* it is saying we have a better alternative 
 		 * Multiple markers at this line 
 		 * - The constructor Integer(int) has been
 		 * deprecated since version 9 and marked for removal 
@@ -13,6 +13,5 @@ public class WrapperClass {
 		 * variable num1 is not used
 		 */
 		System.out.println(num);
-		
 	}
 }
