f12b1f8a3be2213c1cbc3b6d1ba7d24d8cda2c9d
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-29 12:03:12 +0530

    01-interface

--- java_basictoadvance/Oops/java_basictoadvance_Objects/NeedOfInterface.java
+++ java_basictoadvance/Oops/java_basictoadvance_Objects/NeedOfInterface.java
@@ -1,2 +1,21 @@
 package java_basictoadvance_Objects;
+class Laptop{
+	public void code() {//--0
+		System.out.println("code, compile, run");
+	}
+}
+class Developer {
+	public void devapp(Laptop lap) {
+		//System.out.println("coding..");
+		lap.code();//--0
+	}
+}
 
+public class NeedOfInterface {
+	public static void main(String[] args) {
+		Laptop lap=new Laptop();//--0
+		Developer nikhil= new Developer();
+		nikhil.devapp(lap);//--0
+	}
+	/* output: code, compile, run */
+}
