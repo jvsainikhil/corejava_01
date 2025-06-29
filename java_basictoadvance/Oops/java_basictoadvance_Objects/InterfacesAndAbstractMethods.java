fda60d6c5f766af76d4a556deb46fbabf443b2e9
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-29 10:20:54 +0530

    InterfacesAndAbstractMethods_01

---
+++ java_basictoadvance/Oops/java_basictoadvance_Objects/InterfacesAndAbstractMethods.java
@@ -0,0 +1,34 @@
+package java_basictoadvance_Objects;
+
+interface Sample { // we can create abstract and nonabstract methods
+	//we can design some thing
+	//we created hyreachy
+	/* public abstract By default*/ void show();
+	/* public abstract By default*/ void config();
+}
+
+class LogicCode implements Sample{
+
+	@Override
+	public void show() {
+		System.out.println("in the showf");
+	}
+
+	@Override
+	public void config() {
+		System.out.println("in the config");
+		
+	}
+	
+}
+
+public class InterfacesAndAbstractMethods {
+	public static void main(String[] args) {
+	//	Sample onj=new Sample(); //it is an interface so we cannot 
+		Sample obj;
+		obj = new LogicCode();
+		LogicCode obj1 = new LogicCode();
+		obj.config();
+		obj.show();
+	}
+}
