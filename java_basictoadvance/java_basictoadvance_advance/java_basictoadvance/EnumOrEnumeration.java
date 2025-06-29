c1833ee7bb145527f5ce4d89108397eebdb929c7
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-29 23:21:59 +0530

    EnumOrEnumeration_01

---
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance/EnumOrEnumeration.java
@@ -0,0 +1,22 @@
+package java_basictoadvance;
+
+
+enum Status{ //prebuild
+	Running, Failed, Pending, Success; // all this are objects //named constants
+   //  0   |    1   |   2   |     3   |
+}
+
+public class EnumOrEnumeration {
+	// send a request --> in waht ways it is accepting the request 200,404...etc
+	public static void main(String[] args) {
+		
+		Status s = Status.Running;
+		Status s1 = Status.Failed;
+		Status s2 = Status.Pending;
+		Status s3 = Status.Success;
+		System.out.println(s);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+}
