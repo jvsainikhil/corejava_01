a51d3c6dae456a3944c0041968a6ceff8b33cfab
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-29 12:08:20 +0530

    02-Interface

--- java_basictoadvance/Oops/java_basictoadvance_Objects/NeedOfInterface.java
+++ java_basictoadvance/Oops/java_basictoadvance_Objects/NeedOfInterface.java
@@ -1,11 +1,18 @@
 package java_basictoadvance_Objects;
+
 class Laptop{
 	public void code() {//--0
 		System.out.println("code, compile, run");
 	}
 }
+
+class Desktop{
+	public void code() {
+		System.out.println("code, compile, run: faster");
+	}
+}
 class Developer {
-	public void devapp(Laptop lap) {
+	public void devapp(Laptop lap) { //issue
 		//System.out.println("coding..");
 		lap.code();//--0
 	}
@@ -14,8 +21,9 @@ class Developer {
 public class NeedOfInterface {
 	public static void main(String[] args) {
 		Laptop lap=new Laptop();//--0
+		Desktop desk=new Desktop();
 		Developer nikhil= new Developer();
-		nikhil.devapp(lap);//--0
+		nikhil.devapp(desk);//--0 //issue
 	}
 	/* output: code, compile, run */
 }
