b02aaa9e0ee703c98deadf7e40d82d3f0334b093
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-30 09:16:40 +0530

    Enum_switch

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_Enum/EnumIfSwitch.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_Enum/EnumIfSwitch.java
@@ -1,9 +1,9 @@
 package java_basictoadvance_Enum;
 
-enum compareStatus{ //prebuild
+enum compareStatus { // prebuild
 	Running, Failed, Pending, Success;
-   //  0   |    1   |   2   |     3   |
-  //ordinal---> order 
+	// 0 | 1 | 2 | 3 |
+	// ordinal---> order
 }
 
 public class EnumIfSwitch {
@@ -11,14 +11,30 @@ public class EnumIfSwitch {
 		
 		compareStatus s=compareStatus.Running;
 		
-		//we are comparing 
-		if (s==compareStatus.Running)
-			System.out.println("All good");
-		else if (s==compareStatus.Failed) 
-			System.out.println("Try again");
-		else if (s==compareStatus.Pending) 
-			System.out.println("Please wait");
-		else 
+		switch(s) {
+		case Running:
+			System.out.println("All Good");
+			break;
+		case Failed:
+	      	System.out.println("Try Again");
+	      	break;
+		case Pending:
+			System.out.println("Please Wait");
+	     	break;
+		default:
 			System.out.println("Done");
+		}
 	}
 }
+
+//		
+//		//we are comparing 
+//		if (s==compareStatus.Running)
+//			System.out.println("All good");
+//		else if (s==compareStatus.Failed) 
+//			System.out.println("Try again");
+//		else if (s==compareStatus.Pending) 
+//			System.out.println("Please wait");
+//		else 
+//			System.out.println("Done");
+//	}
