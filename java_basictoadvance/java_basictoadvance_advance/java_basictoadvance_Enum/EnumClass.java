827a83cad26484d7ef867f2daa8dfbe8e2b8b380
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-06-30 13:53:09 +0530

    Enum pricing
    assigning values

---
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_Enum/EnumClass.java
@@ -0,0 +1,28 @@
+package java_basictoadvance_Enum;
+
+enum LaptopPurchase { // prebuild
+	Mackbook(2000), Xps(2200), Surface(1500),ThinkPad(1800);
+    private int price;
+
+	//Constructor using Fields
+	private LaptopPurchase(int price) {
+		this.price = price;
+	}
+     //----------------------------------------
+	//Getters and Setters
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
+
+//Extends not work in enum but we can create constructors & variables
+public class EnumClass {
+	public static void main(String[] args) {
+		LaptopPurchase lap = LaptopPurchase.Mackbook;
+		System.out.println(lap + " : "+ lap.getPrice());
+	} 
+}
