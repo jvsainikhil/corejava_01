e4c0b36656b3b33c81f2d1b25c81bcabf4cac961
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-02 16:04:28 +0530

    Exception_03

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
@@ -1,5 +1,12 @@
 package java_basictoadvance_exception;
 
+class NikhilException extends Exception
+{
+	public NikhilException(String string) {
+		
+	}
+}
+
 public class ExceptionsExplained {
 
 	public static void main(String[] args) {
@@ -8,9 +15,12 @@ public class ExceptionsExplained {
 
 		try {
 			b = 18 / a;
-		} catch (ArithmeticException e)  {
+			if(b==0) // used to throw the exception 
+				//throw new ArithmeticException("I dont want to print 0");
+				throw new NikhilException(" dont want to print 0");
+		} catch (NikhilException e)  {
 			b = 18/1;
-			System.out.println("That's the default output");
+			System.out.println("That's the default output" + e);
 		} catch (Exception e) {
 			System.out.println("Some thing went wrong" + e);
 		}
