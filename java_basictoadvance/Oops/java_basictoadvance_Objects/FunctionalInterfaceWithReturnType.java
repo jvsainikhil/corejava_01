5d915c7457aa9dedc2876a1e81081dc7881853e8
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-02 07:41:28 +0530

    Functional interface in 2 int values

--- java_basictoadvance/Oops/java_basictoadvance_Objects/FunctionalInterfaceWithReturnType.java
+++ java_basictoadvance/Oops/java_basictoadvance_Objects/FunctionalInterfaceWithReturnType.java
@@ -7,13 +7,9 @@ interface uauser {
 
 public class FunctionalInterfaceWithReturnType {
 	public static void main(String[] args) {
-		uauser b= new uauser() {
-
-			@Override
-			public int add(int i, int j) {
+		//Functional Interface
+		uauser b= (int i, int j) -> {
 				return i+j;
-			}
-			
 		};
 		int result = b.add(30, 40);
 		System.out.println(result);
