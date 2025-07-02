15da99977542fdc3b24c3eb40b72602daff2485b
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-02 11:26:53 +0530

    exception_03

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
@@ -3,11 +3,14 @@ package java_basictoadvance_exception;
 public class ExceptionsExplained {
 
 	public static void main(String[] args) {
-		int a = 0;
+		int a = 2;
 		int b = 0;// before the value is 0 so the output comes as 0
 		int num[] = new int[5];
+		String str =null;
+		
 		try {
 			b = 18 / a;
+			System.out.println(str.length());
 			System.out.println(num[1]);
 			System.out.println(num[5]);
 			
@@ -17,6 +20,8 @@ public class ExceptionsExplained {
 		} catch (ArrayIndexOutOfBoundsException e)  {
 			//we are printing exception as well
 			System.out.println("Stay in the num");
+		} catch (Exception e) {
+			System.out.println("Some thing went wrong" + e);
 		}
 		System.out.println(b);// before the value is 0 so the output comes as 0
 		System.out.println("bye");
