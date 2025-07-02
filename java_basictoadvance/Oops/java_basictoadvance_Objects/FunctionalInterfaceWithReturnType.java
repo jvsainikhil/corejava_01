fc217752e8927f64e9eca83325372904e1702801
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-02 07:44:29 +0530

    Done

--- java_basictoadvance/Oops/java_basictoadvance_Objects/FunctionalInterfaceWithReturnType.java
+++ java_basictoadvance/Oops/java_basictoadvance_Objects/FunctionalInterfaceWithReturnType.java
@@ -1,13 +1,13 @@
 package java_basictoadvance_Objects;
 
-@FunctionalInterface
+@FunctionalInterface 
 interface uauser {
 	 int  add(int i, int j);
 }
 
 public class FunctionalInterfaceWithReturnType {
 	public static void main(String[] args) {
-		//Functional Interface
+		//Functional Interface - Lamda Expression
 		uauser b= (i, j) -> i+j;
 		int result = b.add(30, 40);
 		System.out.println(result);
