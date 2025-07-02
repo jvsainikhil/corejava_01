ffb3fc1555b622122ba125e49033357877c07832
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-02 11:21:07 +0530

    exception_02

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
@@ -5,11 +5,18 @@ public class ExceptionsExplained {
 	public static void main(String[] args) {
 		int a = 0;
 		int b = 0;// before the value is 0 so the output comes as 0
+		int num[] = new int[5];
 		try {
 			b = 18 / a;
-		} catch (Exception e) {
+			System.out.println(num[1]);
+			System.out.println(num[5]);
+			
+		} catch (ArithmeticException e)  {
 			//we are printing exception as well
-			System.out.println("Something went wrong: " + e);
+			System.out.println("Cannot divide by 0");
+		} catch (ArrayIndexOutOfBoundsException e)  {
+			//we are printing exception as well
+			System.out.println("Stay in the num");
 		}
 		System.out.println(b);// before the value is 0 so the output comes as 0
 		System.out.println("bye");
