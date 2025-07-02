dc81f43c155f8118b141cbce0ef06940719ac806
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-02 10:44:44 +0530

    Exception_01

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
@@ -3,6 +3,14 @@ package java_basictoadvance_exception;
 public class ExceptionsExplained {
 
 	public static void main(String[] args) {
-		
+		int a = 0;
+		int b = 0;// before the value is 0 so the output comes as 0
+		try {
+			b = 18 / a;
+		} catch (Exception e) {
+			System.out.println("Something went wromg");
+		}
+		System.out.println(b);// before the value is 0 so the output comes as 0
+		System.out.println("bye");
 	}
 }
