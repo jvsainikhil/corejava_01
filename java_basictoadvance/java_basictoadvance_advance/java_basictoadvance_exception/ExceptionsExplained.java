afbf6f99fe3bd7b3fac4bd937a5577176170f5d0
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-02 15:58:47 +0530

    catch block

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_exception/ExceptionsExplained.java
@@ -3,23 +3,14 @@ package java_basictoadvance_exception;
 public class ExceptionsExplained {
 
 	public static void main(String[] args) {
-		int a = 2;
+		int a = 20;
 		int b = 0;// before the value is 0 so the output comes as 0
-		int num[] = new int[5];
-		String str =null;
-		
+
 		try {
 			b = 18 / a;
-			System.out.println(str.length());
-			System.out.println(num[1]);
-			System.out.println(num[5]);
-			
 		} catch (ArithmeticException e)  {
-			//we are printing exception as well
-			System.out.println("Cannot divide by 0");
-		} catch (ArrayIndexOutOfBoundsException e)  {
-			//we are printing exception as well
-			System.out.println("Stay in the num");
+			b = 18/1;
+			System.out.println("That's the default output");
 		} catch (Exception e) {
 			System.out.println("Some thing went wrong" + e);
 		}
