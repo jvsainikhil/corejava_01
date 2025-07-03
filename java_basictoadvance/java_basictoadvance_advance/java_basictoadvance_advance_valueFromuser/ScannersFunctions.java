b2f97b259ed1f321800c5f2ddaa156f3534cf1ce
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-03 23:23:23 +0530

    scanner function ASCII

---
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_valueFromuser/ScannersFunctions.java
@@ -0,0 +1,11 @@
+package java_basictoadvance_advance_valueFromuser;
+
+import java.io.IOException;
+
+public class ScannersFunctions {
+	public static void main(String[] args) throws IOException {
+		System.out.println("Enter a number");
+		int num = System.in.read();
+		System.out.println(num);
+	}
+}
