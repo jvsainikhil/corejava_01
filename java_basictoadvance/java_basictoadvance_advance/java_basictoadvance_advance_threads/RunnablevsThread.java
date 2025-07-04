c9188926fbc960d3ee5095b6cd857292eabd157e
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-04 19:02:49 +0530

    with lamda expression

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_threads/RunnablevsThread.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_threads/RunnablevsThread.java
@@ -1,36 +1,56 @@
 package java_basictoadvance_advance_threads;
 
 
-class Xx implements Runnable{
-	public void run() {
-		for(int i=1;i<=5;i++ )
-		System.out.println("hi");
-		try {
-			Thread.sleep(10); 
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-}
+//class Xx implements Runnable{
+//	public void run() {
+//		for(int i=1;i<=5;i++ )
+//		System.out.println("hi");
+//		try {
+//			Thread.sleep(100); 
+//			Thread.sleep(10); 
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
 
 
-class Yy implements Runnable {
-	public void run() {
-		for(int i=1;i<=5;i++ )
-		System.out.println("hello");
-		try {
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} 
-	}
-}
+//class Yy implements Runnable {
+//	public void run() {
+//		for(int i=1;i<=5;i++ )
+//		System.out.println("hello");
+//		try {
+//			Thread.sleep(2);
+//			Thread.sleep(10);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		} 
+//	}
+//}
 
 
 public class RunnablevsThread {
 	public static void main(String[] args) {
-		Runnable ab= new X();
-		Runnable a= new Y();
+		Runnable ab= () -> {
+				for(int i=1;i<=5;i++ )
+				System.out.println("hi");
+				try {
+					Thread.sleep(100); 
+					Thread.sleep(10); 
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+		};
+		Runnable a= () -> {
+			for(int i=1;i<=5;i++ )
+			System.out.println("hello");
+			try {
+				Thread.sleep(100); 
+				Thread.sleep(10); 
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	};
 		//ab.start();
 		//a.start();
 	    Thread t1 = new Thread(ab);
