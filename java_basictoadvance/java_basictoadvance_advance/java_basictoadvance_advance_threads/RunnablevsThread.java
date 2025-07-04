130f97e7d38955e0bcc759ae74240549300d0fff
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-04 18:00:12 +0530

    threads implemet and run

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_threads/RunnablevsThread.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_threads/RunnablevsThread.java
@@ -1,10 +1,9 @@
 package java_basictoadvance_advance_threads;
 
-class Z{
-}
+
 class Xx implements Runnable{
 	public void run() {
-		for(int i=1;i<=100;i++ )
+		for(int i=1;i<=5;i++ )
 		System.out.println("hi");
 		try {
 			Thread.sleep(100); 
@@ -15,9 +14,9 @@ class Xx implements Runnable{
 }
 
 
-class Yy implements Runnable{
+class Yy implements Runnable {
 	public void run() {
-		for(int i=1;i<=100;i++ )
+		for(int i=1;i<=5;i++ )
 		System.out.println("hello");
 		try {
 			Thread.sleep(2);
@@ -32,7 +31,12 @@ public class RunnablevsThread {
 	public static void main(String[] args) {
 		X ab= new X();
 		Y a= new Y();
-		ab.start();
-		a.start();
+		//ab.start();
+		//a.start();
+	    Thread t1 = new Thread(ab);
+	    Thread t2 = new Thread(a);
+	    t1.start();
+	    t2.start();
+	    
 	}
 }
