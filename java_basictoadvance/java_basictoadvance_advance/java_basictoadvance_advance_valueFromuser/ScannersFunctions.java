c87e5ee438a0baf87c61a905a21ab6824adc6512
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-04 13:40:51 +0530

    scanner will give int value

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_valueFromuser/ScannersFunctions.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_valueFromuser/ScannersFunctions.java
@@ -1,11 +1,17 @@
 package java_basictoadvance_advance_valueFromuser;
 
+import java.io.FilterOutputStream;
 import java.io.IOException;
 
 public class ScannersFunctions {
 	public static void main(String[] args) throws IOException {
+		//it is printing an assici value
+		//public class PrintStream extends FilterOutputStream
 		System.out.println("Enter a number");
 		int num = System.in.read();
-		System.out.println(num);
+		System.out.println(num); //ASCII
 	}
 }
+
+
+
