6846558015e3042c66d5f4e1d2c132101283e6be
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-04 14:10:10 +0530

    Bufferreader

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_valueFromuser/ScannersFunctions.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_valueFromuser/ScannersFunctions.java
@@ -1,15 +1,21 @@
 package java_basictoadvance_advance_valueFromuser;
 
-import java.io.FilterOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ScannersFunctions {
 	public static void main(String[] args) throws IOException {
 		//it is printing an assici value
 		//public class PrintStream extends FilterOutputStream
 		System.out.println("Enter a number");
-		int num = System.in.read();
+		
+		InputStreamReader in =new InputStreamReader(System.in);
+		BufferedReader bf = new BufferedReader(in);
+		
+		int num = Integer.parseInt(bf.readLine());
 		System.out.println(num); //ASCII
+		bf.close();
 	}
 }
 
