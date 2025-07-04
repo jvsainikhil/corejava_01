0158ddc065fafdafb907b32003057ea828a3b90b
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-04 14:13:20 +0530

    scanner function

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_valueFromuser/ScannersFunctions.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_valueFromuser/ScannersFunctions.java
@@ -1,23 +1,19 @@
 package java_basictoadvance_advance_valueFromuser;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class ScannersFunctions {
-	public static void main(String[] args) throws IOException {
-		//it is printing an assici value
-		//public class PrintStream extends FilterOutputStream
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// it is printing an assici value
+		// public class PrintStream extends FilterOutputStream
 		System.out.println("Enter a number");
-		
-		InputStreamReader in =new InputStreamReader(System.in);
-		BufferedReader bf = new BufferedReader(in);
-		
-		int num = Integer.parseInt(bf.readLine());
-		System.out.println(num); //ASCII
-		bf.close();
+		// Difficult way
+		// InputStreamReader in = new InputStreamReader(System.in);
+		// BufferedReader bf = new BufferedReader(in);
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(num); // ASCII
+		//bf.close();
 	}
 }
-
-
-
