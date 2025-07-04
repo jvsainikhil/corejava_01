b4f82c94ce759675c41d6a772727e8ad5bb06390
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-04 17:22:57 +0530

    setting priority for tread

--- java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_threads/WhatareThreads.java
+++ java_basictoadvance/java_basictoadvance_advance/java_basictoadvance_advance_threads/WhatareThreads.java
@@ -1,6 +1,6 @@
 package java_basictoadvance_advance_threads;
 
-// wanted to print parallely 
+// we wanted to print parallely 
 class X extends Thread{
 	public void run() {// run threads
 		for(int i=1;i<=100;i++ )
@@ -21,6 +21,13 @@ public class WhatareThreads {
 	public static void main(String[] args) {
 		X ab= new X();// we are making objects as threads
 		Y a= new Y();// we are making objects as threads
+		
+		//printing priorityj
+		a.setPriority(Thread.MAX_PRIORITY); // we can only suggest 
+		System.out.println(ab.getPriority()); 
+		// output:5 --->
+		// The priority ranges from 1 to 10 ---> 5 is default
+		
 		ab.start();// this will call run method
 		a.start();// this will call run method
 	}
