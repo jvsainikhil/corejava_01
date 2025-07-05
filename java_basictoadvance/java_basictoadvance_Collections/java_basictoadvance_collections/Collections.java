8c2d601b25aecf3a7948f6c8761981f8f44f234e
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 11:45:30 +0530

    Collections_01

---
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/Collections.java
@@ -0,0 +1,21 @@
+package java_basictoadvance_collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		Collection nums = new ArrayList();
+		nums.add(10);
+		nums.add(20);
+		nums.add(30);
+		nums.add(40);
+		nums.add(50);
+		
+		System.out.println(nums);
+		
+		
+	}
+
+}
