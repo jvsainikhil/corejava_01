7d664948b40c4d5a3a2e7f91331a3fa9255d9ae0
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 15:53:21 +0530

    map

---
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/MapInCollections.java
@@ -0,0 +1,22 @@
+package java_basictoadvance_collections;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapInCollections {
+
+	public static void main(String[] args) {
+		Map<String,Integer> eceSection= new LinkedHashMap<String,Integer>();
+		eceSection.put("Nikhil", 1);
+		eceSection.put("Jvs", 2);
+		eceSection.put("sai", 3);	
+		System.out.println(eceSection);
+		
+		Map<String,Integer> cseSection= new HashMap<String,Integer>();
+		cseSection.put("king", 1);
+		cseSection.put("queen", 2);
+		cseSection.put("sword", 3);
+		System.out.println(cseSection);
+	}
+}
