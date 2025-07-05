9b79af03489d9101878229e5d52d2db6ac1a5926
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 17:15:37 +0530

    keeping one side is ok

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/MapInCollections.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/MapInCollections.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class MapInCollections {
 
 	public static void main(String[] args) {
-		Map<String,Integer> eceSection= new LinkedHashMap<String,Integer>();
+		Map<String,Integer> eceSection= new LinkedHashMap<>();// keeping any one side is ok
 		//eceSection.put("Nikhil", 1);
 		eceSection.put("Nikhil", 1);
 		eceSection.put("Jvs", 2);
@@ -21,7 +21,7 @@ public class MapInCollections {
 		}
 		//keys are set not accepts duplicate
 		//values are list accepts duplicate
-		Map<String,Integer> cseSection= new Hashtable<String,Integer>();
+		Map<String,Integer> cseSection= new Hashtable<>();// keeping any one side is ok
 	//	cseSection.put("king", 1);
 		cseSection.put("king", 1);
 		cseSection.put("queen", 2);
