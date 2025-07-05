400a02f18518d96ed9a68de92a838698379795e3
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 22:04:16 +0530

    Student age wit string and numbers

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/CompatorvsComparableExample2.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/CompatorvsComparableExample2.java
@@ -5,23 +5,53 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+class Student implements Comparable<Student>{
+	int age;
+	String name;
+	
+	public Student(int age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+
+	public String toString() {
+		return "Student [age=" + age + ", name=" + name + "]";
+	}
+
+	//this and that
+	public int compareTo(Student that) {
+		if (this.age > that.age)
+			return 1;
+		else
+			return -1;
+	}
+	
+}
+
 public class CompatorvsComparableExample2 {
 	public static void main(String[] args) {
-		Comparator<Integer> com = new Comparator<>() {
+		Comparator<Student> com = new Comparator<>() {
 			@Override
-			public int compare(Integer i, Integer j) {
-				if (i % 10 > j % 10)
+			public int compare(Student i, Student j) {
+				if (i.age > j.age)
 					return 1;
 				else
 					return -1;
 			}
 		};
-		List<Integer> nums = new ArrayList<>();
-		nums.add(21);
-		nums.add(25);
-		nums.add(92);
-		nums.add(44);
-		nums.add(13);
-		Collections.sort(nums, com);// pass object
+		
+		List<Student> studs = new ArrayList<>();
+		studs.add(new Student(21, "Nikhil"));
+		studs.add(new Student(20, "Srikanth"));
+		studs.add(new Student(32, "vasavi"));
+		studs.add(new Student(29, "Rakshita"));
+		
+		Collections.sort(studs, com);// pass object
+		for (Student i : studs) {
+			System.out.println(i);
+		}
+		
+		
 	}
 }
