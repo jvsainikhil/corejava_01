e4d5736ced52d4ee99bfbf4a25aef3920533bd81
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 17:08:49 +0530

    will get all the keys from Map different order

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/MapInCollections.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/MapInCollections.java
@@ -8,21 +8,28 @@ public class MapInCollections {
 
 	public static void main(String[] args) {
 		Map<String,Integer> eceSection= new LinkedHashMap<String,Integer>();
+		//eceSection.put("Nikhil", 1);
 		eceSection.put("Nikhil", 1);
-		eceSection.put("Nikhil", 0);
 		eceSection.put("Jvs", 2);
 		eceSection.put("sai", 3);	
-		System.out.println(eceSection);
+		// will get all the keys 1st process
+		System.out.println(eceSection.keySet()+":"+eceSection.values());
+		// will get all the keys 2nd process
+		System.out.println();
+		
+		for(String studentname:eceSection.keySet()) {
+			System.out.println(studentname+" : "+ eceSection.get(studentname));
+		}
+		
 		
 		//keys are set not accepts duplicate
 		//values are list accepts duplicate
-		
 		Map<String,Integer> cseSection= new HashMap<String,Integer>();
+	//	cseSection.put("king", 1);
 		cseSection.put("king", 1);
-		cseSection.put("king", 0);
 		cseSection.put("queen", 2);
 		cseSection.put("sword", 3);
-		System.out.println(cseSection);
-		System.out.println(cseSection.get("king"));
+		//System.out.println(cseSection);
+	//	System.out.println(cseSection.get("king"));
 	}
 }
