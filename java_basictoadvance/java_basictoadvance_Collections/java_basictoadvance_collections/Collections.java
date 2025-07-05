3dffb929740a347b45446c0b409ce7536f84c177
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 12:22:05 +0530

    ArrayList

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/Collections.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/Collections.java
@@ -6,18 +6,20 @@ import java.util.Collection;
 public class Collections {
 
 	public static void main(String[] args) {
+		//Array List is a class which Implements List
 		Collection<Integer> nums = new ArrayList<Integer>();
 		nums.add(10); // object types
 		nums.add(20);
 		nums.add(30);
 		nums.add(40);
-		nums.add(50);
+		//nums.add("nki");
 		
-		for (int n :nums) {
+		for (int n:nums) {
 			System.out.println(n);
 		}
-		
-		
 	}
-
 }
+
+
+
+
