098300a033c82b90c8cb625ac5a9d24e497f0760
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 15:04:58 +0530

    Set will not have duplicate values

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/SetinJava.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/SetinJava.java
@@ -1,21 +1,21 @@
 package java_basictoadvance_collections;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SetinJava {
 
 	public static void main(String[] args) {
-		List<Integer> nums = new ArrayList<Integer>();
+		Set<Integer> nums = new HashSet<Integer>();
 		nums.add(1);
 		nums.add(2);
+		nums.add(4);
 		nums.add(3);
 		nums.add(4);
 		nums.add(1);
-//o/p:1 2 3 4 1 
-		for (Object i : nums) {
-			System.out.print(i + " ");
+//o/p:1 2 3 4  --> no multiple values, we will not have get
+		for (int i : nums) {
+			System.out.print(i);
 		}
 	}
 }
