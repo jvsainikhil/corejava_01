7fe073b96f09f3532311abe99a85c479380db157
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 12:34:24 +0530

    Getting index from List

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/Collections.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/Collections.java
@@ -1,23 +1,24 @@
 package java_basictoadvance_collections;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 public class Collections {
 
 	public static void main(String[] args) {
 		//Array List is a class which Implements List
 		//Collection is just to fetch values 
-		Collection<Integer> nums = new ArrayList<Integer>();
+		List<Integer> nums = new ArrayList<Integer>();
 		nums.add(10); // object types
 		nums.add(20);
 		nums.add(30);
 		nums.add(40);
-		//nums.add("nki");
 		
-		for (int n:nums) {
-			System.out.println(n);
-		}
+		System.out.println(nums.get(2));
+		
+//		for (int n:nums) {
+//			System.out.println(n);
+//		}
 	}
 }
 
