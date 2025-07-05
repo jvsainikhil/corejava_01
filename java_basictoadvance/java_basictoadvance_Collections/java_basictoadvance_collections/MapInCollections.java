9a31f20c1a7c11b933f459743a85d8f84d844828
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 15:58:03 +0530

    Map_02

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/MapInCollections.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/MapInCollections.java
@@ -9,14 +9,20 @@ public class MapInCollections {
 	public static void main(String[] args) {
 		Map<String,Integer> eceSection= new LinkedHashMap<String,Integer>();
 		eceSection.put("Nikhil", 1);
+		eceSection.put("Nikhil", 0);
 		eceSection.put("Jvs", 2);
 		eceSection.put("sai", 3);	
 		System.out.println(eceSection);
 		
+		//keys are set not accepts duplicate
+		//values are list accepts duplicate
+		
 		Map<String,Integer> cseSection= new HashMap<String,Integer>();
 		cseSection.put("king", 1);
+		cseSection.put("king", 0);
 		cseSection.put("queen", 2);
 		cseSection.put("sword", 3);
 		System.out.println(cseSection);
+		System.out.println(cseSection.get("king"));
 	}
 }
