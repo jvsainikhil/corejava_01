2b6269966b93b86e7c9b2f6a2762cf52b00a774e
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-05 15:06:48 +0530

    Treeset No duplicate values, will give in sorted form

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/SetinJava.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/SetinJava.java
@@ -1,19 +1,20 @@
 package java_basictoadvance_collections;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetinJava {
 
 	public static void main(String[] args) {
-		Set<Integer> nums = new HashSet<Integer>();
+		Set<Integer> nums = new TreeSet<Integer>();
 		nums.add(1);
 		nums.add(2);
 		nums.add(4);
 		nums.add(3);
 		nums.add(4);
 		nums.add(1);
-//o/p:1 2 3 4  --> no multiple values, we will not have get
+//o/p:1 2 3 4  --> no multiple values, we will not have get,
+		//in soreted way we will be getting the values
 		for (int i : nums) {
 			System.out.print(i);
 		}
