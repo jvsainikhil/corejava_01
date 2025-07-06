b5549fa6cd5a2a44c56ac0435f5f38c5be413e2b
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 21:14:25 +0530

    Array lo num with var

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
@@ -3,7 +3,7 @@ package java_basictoadvance_java10_17;
 // LVIT - Local variable type inference
 //These var is only applicable for local variable
 public class Update_java10 {
-	var c = 20;// not works
+	//var c = 20;// not works
 	/*
 	 * Exception in thread "main" java.lang.Error: Unresolved compilation problem:
 	 * at java_basictoadvance_java10_17.Update_java10.main(Update_java10.java:8)
@@ -12,13 +12,18 @@ public class Update_java10 {
 	public static void main(String[] args) {
 		int i = 20;
 		var j = 30; // Assigned at compiler time
-		int c;
+		var name="Nikhil";
+		//int c;
 		// var d;// not works
 		/*
 		 * Exception in thread "main" java.lang.Error: Unresolved compilation problem:
 		 * Cannot use 'var' on variable without initializer at
 		 * java_basictoadvance_java10_17.Update_java10.main(Update_java10.java:11)
 		 */
-		System.out.println(i + j);
+		System.out.println(i + j+"\n"+name);
+		
+		int nums[] = new int[10];
+		
+		var nums1 = new int[10];
 	}
 }
