8011c0181b36e4d101456f9f29eba236b14b0e6f
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 21:10:35 +0530

    error
    Exception in thread "main" java.lang.Error: Unresolved compilation
    problem:
    
    	at java_basictoadvance_java10_17.Update_java10.main(Update_java10.java:8)

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
@@ -1,13 +1,24 @@
 package java_basictoadvance_java10_17;
 
-
 // LVIT - Local variable type inference
 //These var is only applicable for local variable
 public class Update_java10 {
+	var c = 20;// not works
+	/*
+	 * Exception in thread "main" java.lang.Error: Unresolved compilation problem:
+	 * at java_basictoadvance_java10_17.Update_java10.main(Update_java10.java:8)
+	 */
+
 	public static void main(String[] args) {
-		int i=20; 
-		var j=30; //Assigned at compiler time 
+		int i = 20;
+		var j = 30; // Assigned at compiler time
 		int c;
-		System.out.println(i+j);
+		// var d;// not works
+		/*
+		 * Exception in thread "main" java.lang.Error: Unresolved compilation problem:
+		 * Cannot use 'var' on variable without initializer at
+		 * java_basictoadvance_java10_17.Update_java10.main(Update_java10.java:11)
+		 */
+		System.out.println(i + j);
 	}
 }
