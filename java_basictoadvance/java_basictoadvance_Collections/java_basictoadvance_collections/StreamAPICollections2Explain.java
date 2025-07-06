4336418169b96d5717149f0c08374452d7649981
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 19:34:17 +0530

    Filter_Predicate_01

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2Explain.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2Explain.java
@@ -2,38 +2,28 @@ package java_basictoadvance_collections;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.function.Predicate;
 
 public class StreamAPICollections2Explain {
 
 	public static void main(String[] args) {
-		List<Integer> nums = Arrays.asList(4, 8, 5, 1);// original values
-
-		// once we use it we cannot use it again this is an interface
-		// all operations can be performed here s1
+		List<Integer> nums = Arrays.asList(4, 8, 5, 1);
 		
-		/* In one line we can write this */
-//		Stream<Integer> s1 =nums.stream(); //we will get all the values on s1
-//		Stream<Integer> s2 = s1.filter(n -> n%2==0);
-//		Stream<Integer> s3 = s2.map(n -> n*2);
-//		int result=s3.reduce(0,(c,e)-> c+e);
+		Predicate<Integer> p=new  Predicate<Integer>() { //--->01
+			
+			public boolean test(Integer n) {
+				if(n%2==0)
+					return false;
+				else
+					return true;
+			}
+		};
 		
 		int result = nums.stream()
-				.filter(n -> n % 2 == 0)
+				.filter(p/* n -> n % 2 == 0 */)//---> 01
 				.map(n -> n * 2)
 				.reduce(0, (c, e) -> c + e);
 		
 		System.out.println(result);
-		// s3.forEach(n -> System.out.println(n));
-
-		/*
-		 * we can use only once output: 4 8 5 1 Exception in thread "main"
-		 * java.lang.IllegalStateException: stream has already been operated upon or
-		 * closed at java.base/java.util.stream.AbstractPipeline.sourceStageSpliterator(
-		 * AbstractPipeline.java:279) at
-		 * java.base/java.util.stream.ReferencePipeline$Head.forEach(ReferencePipeline.
-		 * java:762) at java_basictoadvance_collections.StreamAPICollections2.main(
-		 * StreamAPICollections2.java:17)
-		 */
 	}
 }
