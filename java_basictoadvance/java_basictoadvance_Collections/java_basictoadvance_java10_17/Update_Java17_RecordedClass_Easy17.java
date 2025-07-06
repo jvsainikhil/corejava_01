43b1358e588094f18ecce9735fe6e7c031ae41a5
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 23:54:06 +0530

    "record" DataHolder(int id, String name)
    -> canonical constructor (int id, String name)
    -> DataHolder - class name
    -> record - key word

---
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_RecordedClass_Easy17.java
@@ -0,0 +1,73 @@
+package java_basictoadvance_java10_17;
+
+import java.util.Objects;
+
+//---> THIS HOLDS DATA
+/*class DataHolder{ 
+	private final int id;
+	private final String name;
+	
+	//To assign value we use constructor 
+	public DataHolder(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	//If we want to fetch the data or convert to jsonm we need to access it
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	//To frint all at once we use tostring
+	public String toString() {
+		return "DataHolders [id=" + id + ", name=" + name + "]";
+	}
+
+	//saying compare just the values don't compare memeory location
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DataHolder other = (DataHolder) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+}
+*/
+
+// we got all the object and it is true Simple One with out doing the top methods 
+record DataHolder(int id, String name) { // --> canonical constructor
+	
+}
+
+public class Update_Java17_RecordedClass_Easy17 {
+
+	public static void main(String[] args) {
+		//Data Carrier Class
+		//if we have come data for store purpose only 
+		DataHolder a1= new DataHolder(1, "Nikhil");
+		DataHolder a2= new DataHolder(1, "Nikhil");
+		System.out.println(a1.equals(a2)); 
+		System.out.println(a1);
+	}
+}
+
+//output: False --> they are not same objects 2different object, because of values we are saying
+// --> 0, We want to get the values
+//Normal way -> bit we want all the values so for this --> 0
+/*var a = a1.getName();
+		var b = a1.getId();
+		System.out.println(b +" : "+ a);*/
