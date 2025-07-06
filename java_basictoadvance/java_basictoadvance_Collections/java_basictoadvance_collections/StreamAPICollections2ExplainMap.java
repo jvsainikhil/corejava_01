e04f62871fcba1ce9d4c29f7e9b86ab48a2cface
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 20:08:27 +0530

    Map_Function_01

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2ExplainMap.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2ExplainMap.java
@@ -2,6 +2,7 @@ package java_basictoadvance_collections;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class StreamAPICollections2ExplainMap {
@@ -9,12 +10,17 @@ public class StreamAPICollections2ExplainMap {
 	public static void main(String[] args) {
 		List<Integer> nums = Arrays.asList(4, 8, 5, 1);
 		
-		Predicate<Integer> p= n -> n%2==0; //--FINAL_01
-	
+		Function<Integer, Integer> fun= new Function<Integer, Integer>() {
+			
+			public Integer apply(Integer n) {
+				
+				return n*2;
+			}
+		};
 		
 		int result = nums.stream()
-				.filter(n -> n % 2 == 0) //--FINAL_01
-				.map(n -> n * 2)
+				.filter(n -> n % 2 == 0) 
+				.map(fun/* n -> n * 2 */)
 				.reduce(0, (c, e) -> c + e);
 		
 		System.out.println(result);
