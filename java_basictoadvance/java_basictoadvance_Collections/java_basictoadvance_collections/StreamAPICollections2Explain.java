d91d10028ad12cf5c362853401936b7ea78a6104
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 17:24:05 +0530

    IGNORE

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2Explain.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2Explain.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class StreamAPICollections2 {
+public class StreamAPICollections2Explain {
 
 	public static void main(String[] args) {
 		List<Integer> nums = Arrays.asList(4, 8, 5, 1);// original values
