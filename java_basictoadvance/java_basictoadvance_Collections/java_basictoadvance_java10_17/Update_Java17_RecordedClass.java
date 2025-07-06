c55c9d16d1d3433c59a21c6fe9ae8516586655f8
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 23:41:34 +0530

    Getting false for equals

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_RecordedClass.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_RecordedClass.java
@@ -1,5 +1,7 @@
 package java_basictoadvance_java10_17;
 
+import java.util.Objects;
+
 class DataHolders{
 	private final int id;
 	private final String name;
@@ -24,6 +26,24 @@ class DataHolders{
 	public String toString() {
 		return "DataHolders [id=" + id + ", name=" + name + "]";
 	}
+
+	//Saying compare just the values
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DataHolders other = (DataHolders) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
 }
 
 public class Update_Java17_RecordedClass {
@@ -32,8 +52,10 @@ public class Update_Java17_RecordedClass {
 		//Data Carrier Class
 		//if we have come data for store purpose only 
 		DataHolders a1= new DataHolders(1, "Nikhil");
+		DataHolders a2= new DataHolders(1, "Nikhil");
 		// --> 0, We want to get the values
-		System.out.println(a1); //output: DataHolders [id=1, name=Nikhil]
+		
+		System.out.println(a1.equals(a2)); //output: False --> they are not same objects 2different object, because of values we are saying 
 		//Normal way -> bit we want all the values so for this --> 0
 		/*var a = a1.getName();
 		var b = a1.getId();
