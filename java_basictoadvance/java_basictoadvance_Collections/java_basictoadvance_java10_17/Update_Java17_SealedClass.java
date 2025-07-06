d67169a8bbe2eff4aee869efdb4813d92fa831d7
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 21:30:29 +0530

    cannot access

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_SealedClass.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_SealedClass.java
@@ -1,10 +1,29 @@
 package java_basictoadvance_java10_17;
 
-public class Update_Java17_SealedClass {
+//Only B&C able to inherit A
+final class A{
+	
+}
 
+class B extends A{
+	
+}
+
+class C{
+	
+}
+
+class D{
+	
+}
+
+public class Update_Java17_SealedClass {
 	public static void main(String[] args) {
-		//Abstract Class
-		//Final Class
+		//Abstract Class - Is ment to be Inherited 
+		//Final Class    - No class can Inherit
+		//If we cant Inheritance but in every class can do it "Restricting"
+		//can mention which are the subclass, which are the interfaces which can inherit from this particular class and Interface 
+		//Keyword: SEALED
 		
 	}
 }
