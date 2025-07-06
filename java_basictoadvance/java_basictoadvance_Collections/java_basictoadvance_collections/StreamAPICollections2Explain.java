60bb8902a9c2ccd1522c033af65a613c68738f27
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 19:36:32 +0530

    Filter_Predicate_03

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2Explain.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2Explain.java
@@ -9,12 +9,8 @@ public class StreamAPICollections2Explain {
 	public static void main(String[] args) {
 		List<Integer> nums = Arrays.asList(4, 8, 5, 1);
 		
-		Predicate<Integer> p=new  Predicate<Integer>() { //--->01
-			
-			public boolean test(Integer n) {
+		Predicate<Integer> p= n -> {
 				return n%2==0;
-					
-			}
 		};
 		
 		int result = nums.stream()
