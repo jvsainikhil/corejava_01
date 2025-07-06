0a9e4f6137a043e095c0ebd64154237b451d6cbc
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 21:18:54 +0530

    Ignore

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_SealedClass.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_SealedClass.java
@@ -1,9 +1,10 @@
 package java_basictoadvance_java10_17;
 
-public class Update_Java17 {
+public class Update_Java17_SealedClass {
 
 	public static void main(String[] args) {
-
+		//Abstract Class
+		//Final Class
+		
 	}
-
 }
