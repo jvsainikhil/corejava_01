1378ffb07296382ab2138b7f250d1c5ba2741f1b
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 21:00:51 +0530

    when we have a large class name

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 // LVIT - Local variable type inference
 public class Update_java10 {
 	public static void main(String[] args) {
+		//when we have a large class name 
 		var obj = new ArrayList();
 
 	}
