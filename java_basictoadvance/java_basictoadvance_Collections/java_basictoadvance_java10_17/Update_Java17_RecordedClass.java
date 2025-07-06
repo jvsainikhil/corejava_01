9f5ec2ba1e71a7f7128f0289268b88e7a99ac70e
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 23:00:39 +0530

    the out put got printed

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_RecordedClass.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_RecordedClass.java
@@ -1,10 +1,44 @@
 package java_basictoadvance_java10_17;
 
+class DataHolders{
+	private final int id;
+	private final String name;
+	
+	//To assign value we use constructor 
+	public DataHolders(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	//If we want to fetch the data or convert to jsonm we need to access it
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	//To frint all at once we use tostring
+	public String toString() {
+		return "DataHolders [id=" + id + ", name=" + name + "]";
+	}
+}
+
 public class Update_Java17_RecordedClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Data Carrier Class
+		//if we have come data for store purpose only 
+		DataHolders a1= new DataHolders(1, "Nikhil");
+		// --> 0, We want to get the values
+		System.out.println(a1);
+		//Normal way -> bit we want all the values so for this --> 0
+		/*var a = a1.getName();
+		var b = a1.getId();
+		System.out.println(b +" : "+ a);*/
+		
 	}
 
 }
