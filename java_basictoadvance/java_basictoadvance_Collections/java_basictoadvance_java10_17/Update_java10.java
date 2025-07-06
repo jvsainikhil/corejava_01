6e7e075a2225753b9b5d4c11e5fbdb9707f2f447
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 20:37:02 +0530

    var

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
@@ -1,7 +1,11 @@
 package java_basictoadvance_java10_17;
 
+import java.util.ArrayList;
+
+// LVIT - Local variable type inference
 public class Update_java10 {
 	public static void main(String[] args) {
-		System.out.println("hello world!");
+		ArrayList obj = new ArrayList();
+
 	}
 }
