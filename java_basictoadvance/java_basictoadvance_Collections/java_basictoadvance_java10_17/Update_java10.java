4df4e72223b07b6677958283ceb36ac2fcc8e2f8
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 21:06:53 +0530

    will not give comple time error

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_java10.java
@@ -5,8 +5,9 @@ package java_basictoadvance_java10_17;
 //These var is only applicable for local variable
 public class Update_java10 {
 	public static void main(String[] args) {
-		var i=20;
-		var j=30;
+		int i=20; 
+		var j=30; //Assigned at compiler time 
+		int c;
 		System.out.println(i+j);
 	}
 }
