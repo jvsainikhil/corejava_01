f0f8cb1fb5f34983ffb9d299c50c6167b7429783
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 22:41:20 +0530

    Final sealed Class and Interface

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_SealedClass.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_java10_17/Update_Java17_SealedClass.java
@@ -2,36 +2,42 @@ package java_basictoadvance_java10_17;
 
 //Only B&C able to inherit A
 //to perimt do like this
-sealed class A extends Thread implements Cloneable permits B,C{
-	
+/*
+ * FINAL
+ * SEALED
+ * NON SEALED
+ */
+sealed class A extends Thread implements Cloneable permits B, C {
+
 }
 
-non-sealed class B extends A { //---> Can access to other class
-	
+non-sealed class B extends A { // ---> Can access to other class
+
 }
 
-final class C extends A{ //---> no one can extend this
-	
+final class C extends A { // ---> no one can extend this
+
 }
 
-class D extends B{ //---> Can access to other class
-	
+class D extends B { // ---> Can access to other class
+
 }
 
-sealed interface X permits Y{ //-->01
-	
+sealed interface X permits Y { // -->01
+
 }
 
-non-sealed interface Y extends X{ //-->01
+non-sealed interface Y extends X { // -->01
 }
 
 public class Update_Java17_SealedClass {
 	public static void main(String[] args) {
-		//Abstract Class - Is ment to be Inherited 
-		//Final Class    - No class can Inherit
-		//If we cant Inheritance but in every class can do it "Restricting"
-		//can mention which are the subclass, which are the interfaces which can inherit from this particular class and Interface 
-		//Keyword: SEALED
-		
+		// Abstract Class - Is ment to be Inherited
+		// Final Class - No class can Inherit
+		// If we cant Inheritance but in every class can do it "Restricting"
+		// can mention which are the subclass, which are the interfaces which can
+		// inherit from this particular class and Interface
+		// Keyword: SEALED
+
 	}
 }
