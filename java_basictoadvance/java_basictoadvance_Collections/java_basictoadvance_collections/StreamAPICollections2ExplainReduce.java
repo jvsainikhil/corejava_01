0a8ea1d9e6abb38dd19409acb0e49a9d0b850216
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-06 20:20:48 +0530

    Another method

--- java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2ExplainReduce.java
+++ java_basictoadvance/java_basictoadvance_Collections/java_basictoadvance_collections/StreamAPICollections2ExplainReduce.java
@@ -2,21 +2,26 @@ package java_basictoadvance_collections;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class StreamAPICollections2ExplainReduce {
 
 	public static void main(String[] args) {
 		List<Integer> nums = Arrays.asList(4, 8, 5, 1);
 		
-		Function<Integer, Integer> fun=  n -> n * 2;
 				
-		int result = nums.stream()
+		Stream<Integer> sortvalues = nums.stream()
 				.filter(n -> n % 2 == 0) 
-				.map(fun/* n -> n * 2 */)
-				.reduce(0, (c, e) -> c + e);
+				.sorted();
+		sortvalues.forEach(n-> System.err.println(n));
+				//.map(n -> n * 2)
+				//if we want to add value 0+4,
+				//carry element
+				//.reduce(0, (c, e) -> c + e);
 		
-		System.out.println(result);
+		//System.out.println(result);
+		
 	}
 }
+
+
