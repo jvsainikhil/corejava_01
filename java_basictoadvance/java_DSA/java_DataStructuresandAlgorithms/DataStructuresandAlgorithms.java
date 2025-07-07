90e39de97384fc2c04d359b278c97724317c7529
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-07-07 14:14:35 +0530

    Starting DSA

---
+++ java_basictoadvance/java_DSA/java_DataStructuresandAlgorithms/DataStructuresandAlgorithms.java
@@ -0,0 +1,9 @@
+package java_DataStructuresandAlgorithms;
+
+public class DataStructuresandAlgorithms {
+
+	public static void main(String[] args) {
+		System.out.println("DataStructuresandAlgorithms");
+	}
+
+}
